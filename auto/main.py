e854a639c0c5d724286f7a9b1288fbbe6b2d2f3e
Linh Nguyen <linh.nguyen@kamora.vn>
2025-10-03 14:26:37 +0700

    auto gui

--- auto/main.py
+++ auto/main.py
@@ -4,4 +4,25 @@ from pywinauto import Application
 app = Application(backend="uia").connect(title_re=".*Voicepeak.*")
 
 dlg = app.top_window()
+# In ra các control để tham khảo
 print(dlg.print_control_identifiers())
+
+# Click nút "File"
+file_btn = dlg.child_window(title="File", control_type="Button")
+file_btn.click_input()
+
+# Nhập text vào ô Edit đầu tiên
+edit_box = dlg.child_window(control_type="Edit")
+edit_box.set_edit_text("Xin chào, đây là test tự động!")
+
+# Chọn ComboBox "Latest"
+combo = dlg.child_window(title="Latest", control_type="ComboBox")
+combo.select("Latest")
+
+# Click nút "Male 2"
+male_btn = dlg.child_window(title="Male 2", control_type="Button")
+male_btn.click_input()
+
+# Lấy giá trị slider "Volume"
+slider = dlg.child_window(control_type="Slider")
+print("Volume value:", slider.get_value())
